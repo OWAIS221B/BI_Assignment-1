c8c14b56aa3c6e8301b61f48011f3f2c1732cc27
Owais <inform.os221@gmail.com>
2025-06-04 01:28:36 +0530

    Feat: Header

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,12 +1,32 @@
-import React from 'react'
-import Meetuplogo from '../../assets/images/Meetup-logo.png'
+import React from "react";
+import { Search } from "lucide-react"; 
+import Meetuplogo from "../../assets/images/Meetup-logo.png";
 
 const Header = () => {
   return (
-    <div>
-      <img className='h-40 w-40 object-contain' src={Meetuplogo} alt="logo" />
-    </div>
-  )
-}
+    <header className="flex items-center justify-between px-6 py-4 shadow-md bg-white">
+      
+      <div className="flex items-center">
+        <img
+          className="h-20 w-auto object-contain"
+          src={Meetuplogo}
+          alt="Meetup Logo"
+        />
+      </div>
 
-export default Header
+      
+      <div className="relative w-64">
+        <span className="absolute inset-y-0 left-3 flex items-center text-gray-500">
+          <Search size={18} />
+        </span>
+        <input
+          type="text"
+          placeholder="Search events by title and tag"
+          className=" text-black pl-10 pr-4 py-2 w-full rounded-lg border border-gray-300 text-sm"
+        />
+      </div>
+    </header>
+  );
+};
+
+export default Header;
