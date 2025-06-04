566be49669c0cddc14b0512343c0d585c2f17cdd
Owais <inform.os221@gmail.com>
2025-06-04 13:02:12 +0530

    Assets added

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import { useState } from 'react'
 import './App.css'
 import Header from './components/Header/Header'
+import Home from './components/home/Home'
 
 function App() {
   return (
     <>
       <Header />
+      <Home />
     </>
   )
 }
